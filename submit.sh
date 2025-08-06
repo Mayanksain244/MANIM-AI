7bf22ebf96071fc7c4dff540573ee35696c30472
Mayanksain244 <mayanksain244@gmail.com>
2025-08-06 20:33:21 +0530

    added requirements.txt

--- submit.sh
+++ submit.sh
@@ -8,6 +8,6 @@
 cd $PBS_O_WORKDIR                                                   # Go to the directory where the job was submitted
 source /home/soft/anaconda3/etc/profile.d/conda.sh                  # Set up conda
 conda init                                                          # Initialize conda (optional)
-conda activate mayank_venv                                        # Activate your conda environment
+conda activate manim_venv                                       # Activate your conda environment
 module load cuda                                                    # Load CUDA (needed for GPU code)
 python3 ./llamaClassifier.py                                                   # Run your Python script
